6c2f43c06cccd9f7c903bc589a8ba6a13bc46466
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-08-20 14:06:25 -0300

    backup

--- lib/files/files.test.ts
+++ lib/files/files.test.ts
@@ -1,34 +1,41 @@
+import { cwd } from "node:process";
+import path from "node:path";
+import { cpSync, rmSync } from "node:fs";
 import { getForFiles } from "./index";
 
-jest.mock("node:fs/promises", () => ({
-    readdir: async (dir: string) => {
-        if (dir === "/rootDir") {
-            return [
-                "file1.txt", 
-                "image.png", 
-                "subdir"
-            ];
-        } else if (dir === "/rootDir/subdir") {
-            return [
-                "file2.png", 
-                "file3.txt"
-            ];
-        }
-        return [];
-    }
-}));
-
 describe("getForFiles", () => {
+    const testDir = path.join(cwd(), "/tests/files/", "/out");
+    const tmpDir = path.join(cwd(), "/tests/files/", "/tmp/files");
+
     beforeEach(() => {
-        jest.resetAllMocks();
+        cpSync(testDir, tmpDir, { recursive: true });
     });
 
-    it("Deve retornar apenas os arquivos do tipo .png", async () => {
-        const result = await getForFiles("/rootDir");
+    afterEach(() => {
+        rmSync(tmpDir, { recursive: true });
+    })
+
+    it("Deve retornar apenas os arquivos do tipo .png", () => {
+        console.log = jest.fn();
+        const result = getForFiles(tmpDir);
 
         expect(result).toEqual([
-            "/rootDir/image.png",
-            "/rootDir/subdir/file2.png"
+            path.join(tmpDir, "/img/favicon.png"),
+            path.join(tmpDir, "/img/home/aboutus.png"),
+            path.join(tmpDir, "/img/home/contact.png"),
+            path.join(tmpDir, "/img/home/hero.png"),
+            path.join(tmpDir, "/img/home/portfolio/bh-1.png"),
+            path.join(tmpDir, "/img/home/portfolio/bh-3.png"),
+            path.join(tmpDir, "/img/home/portfolio/carol-1.png"),
+            path.join(tmpDir, "/img/home/portfolio/carol-2.png"),
+            path.join(tmpDir, "/img/home/portfolio/carol-3.png"),
+            path.join(tmpDir, "/img/home/portfolio/karina-1.png"),
+            path.join(tmpDir, "/img/home/portfolio/michelle-1.png"),
+            path.join(tmpDir, "/img/home/portfolio/michelle-2.png"),
+            path.join(tmpDir, "/img/home/portfolio/pamella-1.png"),
+            path.join(tmpDir, "/img/home/portfolio/pamella-2.png"),
         ]);
+
+        expect(console.log).toHaveBeenCalledWith("Todas as imagens foram selecionadas.");
     });
 });
--- lib/files/index.ts
+++ lib/files/index.ts
@@ -1,27 +1,42 @@
-import { readdir } from "node:fs/promises"
+import { readdirSync, cpSync, rmSync, existsSync } from "node:fs";
 import path from "node:path";
+import { cwd } from "node:process";
 
 export function filter(files:string[]) {
     return files.filter((file) => {
-        if (file.includes("png")) {
-            return true;
-        }
+        return file.includes("png") || 
+                file.includes("jpg");
     })
 }
 
-export async function getForFiles(dir: string) {
-    const dirs = await readdir(dir)
-    let files: string[] = [];
-
-    for (let i = 0; i < dirs.length; i++) {
-        const name = dirs[i];
-        if (name.includes(".")) {
-            files.push(path.join(dir, name));
-        } else {
-            const newFiles = await getForFiles(path.join(dir, name));
-            newFiles.forEach((file) => files.push(file))
+export function getForFiles(dir: string) {
+    function serchingFile (dir: string) {
+        const dirs = readdirSync(dir)
+        let files: string[] = [];
+    
+        for (let i = 0; i < dirs.length; i++) {
+            const name = dirs[i];
+            if (name.includes(".")) {
+                files.push(path.join(dir, name));
+            } else {
+                const newFiles = serchingFile(path.join(dir, name));
+                newFiles.forEach((file) => files.push(file))
+            }
         }
+    
+        return filter(files);
     }
 
-    return filter(files);
+    const result = serchingFile(dir);
+    console.log("Todas as imagens foram selecionadas.");
+    return result;
+}
+
+export function makeBackup(dir: string) {
+    const name = dir.split("/").reverse()[0];
+    const backupDir = path.join(cwd(), "/history", name);
+    const hasExist = existsSync(backupDir);
+    if (hasExist) rmSync(backupDir, { recursive: true });
+    cpSync(dir, backupDir, { recursive: true });
+    console.log("Backup criado em " + backupDir);
 }
